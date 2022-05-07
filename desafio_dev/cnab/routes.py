1670c8050e4495ce0d2abb4cc8ea4479f9481293
edugarcia98 <36543457+edugarcia98@users.noreply.github.com>
2022-05-07 11:47:01 -0300

    Show operations on screen (#3)

--- desafio_dev/cnab/routes.py
+++ desafio_dev/cnab/routes.py
@@ -1,7 +1,8 @@
-from cnab.views import upload_file
+from cnab.views import load_operations, upload_file
 from django.urls import path
 
 
 urlpatterns = [
     path("upload-file/", upload_file, name="upload-file"),
+    path("operations/<str:slug>/", load_operations, name="operations")
 ]
